f3fa08c23f4865e093316d03de70cb91f7353ee6
clusterwebsolutions <clusterwebsolutions@gmail.com>
2016-11-09 03:22:47 +0530

    ready under construction

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Arleen Builders & Decors | Under Construction</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,400" rel="stylesheet">
+
+    <link rel="stylesheet" href="style.css">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="../../assets/js/html5shiv.js"></script>
+    <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+<!-- Wrap all page content here -->
+<div id="wrap">
+
+    <!-- Begin page content -->
+    <div class="container">
+
+
+        <img src="logo.png" class="img-responsive center-block" width="20%" style="margin: 0 auto;">
+        <img src="under_const.png" class="img-responsive center-block" width="50%" style="margin: 0 auto;">
+
+
+    </div>
+</div>
+<div class="for_div">
+    <p class="text-center" style="margin-bottom: 0"><span class="for_text">For further details, please contact:</p>
+    <p class="text-center"><span class="arleen_color">M</span> : 9940358889 | <span class="arleen_color">E</span>  : info@arleenbuilders.com</p>
+
+</div>
+
+<div id="footer">
+    <div class="container">
+        <p class="text-center footer_text">Builders &nbsp;&nbsp; | &nbsp;&nbsp; Decor's &nbsp;&nbsp; | &nbsp;&nbsp; Sports Arena</p>
+
+    </div>
+</div>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
